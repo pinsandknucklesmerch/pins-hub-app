95bc14e64953d6038f970efeb8d3e9d646e02fca
Duncan <cagedchordproductions@gmail.com>
2026-06-05 16:55:16 +0200

    Update pins dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
-    >
+    <html lang="en" className="h-full antialiased dark">
       <body className="min-h-full flex flex-col bg-black text-white selection:bg-red-600 selection:text-white">
         {children}
         <Toaster theme="dark" position="bottom-right" />
